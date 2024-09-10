7b56b352d77a4f44b906cdb6d697f78575f505ce
Ranveer1singh <ranveersinghtomar2000@gmail.com>
2024-09-10 10:32:09 +0530

    creatr login controller for sign in

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 var localStrategy = require("passport-local").Strategy;
 passport.use(new localStrategy(userModel.authenticate()));
 
-exports.registerUser = async (req, res) => {
+exports.registerUser = (req, res) => {
   try {
     const { username, email } = req.body;
     userModel.register(new userModel({username, email}), req.body.password)
@@ -20,3 +20,22 @@ exports.registerUser = async (req, res) => {
     res.status(500).json(new apiError(500, "internal Sever Error"));
   }
 };
+
+exports.login = (req, res, next) =>{
+  passport.authenticate("local", (err, user, info) =>{
+    if(err){
+      return next(err);
+    }
+    if(!user){
+      return res.status(404).json({message : "user not found"})
+      // return res.status(404).json(new apiError(404,"user not found"))
+    }
+
+    req.logIn(user, (err) =>{
+      if(err){
+        return next(err)
+      }
+      return res.status(200).json(new apiResponse(200,user,"User Login Successfully"))
+    })
+  })(req, res, next);
+} 
