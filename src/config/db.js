1e1ebd2181802baf8bcbcc28b2b5734894d96728
ranveer1singh <ranveersinghtomar2000@gmail.com>
2025-01-25 11:59:34 +0530

    changes in config

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
 
 const connect = (URL) => {
-  mongoose.connect(URL);
-
-  const db = mongoose.connection;
-  db.on(
-    "error",
-    console.error.bind(console, "MongoDB Connection Not Established")
-  );
-
-  db.once("open", () => {
-    console.log("Connected to data base");
-  });
+  try {
+    
+    mongoose.connect(URL);
+  
+    const db = mongoose.connection;
+    db.on(
+      "error",
+      console.error.bind(console, "MongoDB Connection Not Established")
+    );
+  
+    db.once("open", () => {
+      console.log("Connected to data base");
+    });
+  } catch (error) {
+    console.log("error", error);
+    
+  }
 };
 
 module.exports = { connect };
